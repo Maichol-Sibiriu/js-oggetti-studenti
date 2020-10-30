0b038f6cd26d28c5ed79c95f57ad4fae995bbea0
Maichol <msibiriu@gmail.com>
2020-10-30 16:27:31 +0100

    creazione array oggetti studenti fatto.

--- js/main.js
+++ js/main.js
@@ -14,4 +14,28 @@ $(document).ready( function() {
      console.log(student[key]);
    }
 
+   // array di oggetto studenti
+   var list = [
+      {
+        name : "maichol",
+        surname : "sibiriu",
+        age : "34",
+      },
+      {
+        name : "marika",
+        surname : "verdi",
+        age : "30",
+      }
+      {
+        name : "mario",
+        surname : "bianchi",
+        age : "27",
+      }
+      {
+        name : "marco",
+        surname : "rossi",
+        age : "24",
+      }
+   ];
+
 });//fine documento.
