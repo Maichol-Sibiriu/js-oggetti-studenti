eda4814e1a16b14ffd99bec9a8cd800f8be3c70d
Maichol <msibiriu@gmail.com>
2020-10-30 16:14:35 +0100

    creazione oggetto con proprieta' e stampa con for in fatta.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+// javascript
 $(document).ready( function() {
 
+   // oggetto studente
+   var student = {
+      name : "maichol",
+      surname : "sibiriu",
+      age : "34",
+   };
+   console.log(student.surname);
 
+   // stampa con for..in delle proprieta'
+   for (var key in student) {
+     console.log(student[key]);
+   }
 
 });//fine documento.
